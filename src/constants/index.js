34a4cfc8b2bf3497412b8e16e35cca6b0ef87c33
Hardikworkz <workzhardik@gmail.com>
2024-07-23 16:14:54 +0530

    first commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,32 +1,36 @@
 
 export const PROJECTS = [
-    {
-      title: "E-Commerce Website",
-    //   image: ,
-      description:"A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-      technologies: ["HTML", "CSS", "React", "Tailwind Css", "Javascript"],
-    },
+    
     {
         title: "Portfolio Website",
-        // image: <img src="/download(1).jpeg" alt="" />,
+        // location:<a href=""></a>,
+        Image: "download(1) ",   
         description:
           "A personal portfolio website showcasing projects, skills, and contact information.",
         technologies: ["HTML", "Tailwind CSS", "React", "JavaScript"],
       },
       {
         title: "Signup/Login Page",
-        // Image:<img src="/projects/OIP(3).jpeg" alt="" />,
-        description:
-          "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+        // location:<a href=""></a>,
+        Image: "OIP(2)",   
+        description:"An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
         technologies: ["HTML", "CSS", "JavaScript", "React","Tailwind Css"],
       },
       
     {
       title: "Tic-Tac-Toe",
-      // picture: <img src="https://th.bing.com/th/id/OIP.65mcF0q2ryd3nb3NNtw1ogHaFK?rs=1&pid=ImgDetMain" alt="" /> ,
+      // location:<a href=""></a> ,
+      Image: " OIP(3) ",   
       description:
         "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
       technologies: ["HTML", "CSS", "JavaScript"],
+    },
+    {
+      title: "Image Slider",
+    //  location:<a href=""></a>,
+    Image: "Untitled design.png ",   
+      description:"A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+      technologies: ["HTML", "CSS","Javascript"],
     },
   ];
   
