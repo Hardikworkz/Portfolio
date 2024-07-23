4ffe7217fd826e775a27cee81ff39cfd72329b52
Hardikworkz <workzhardik@gmail.com>
2024-07-23 16:26:19 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
+import logoo from "../components/logo.png"
+
 
 const Navbar = () => {
     return (
       <nav className='h-1/3 mb-20 flex items-center justify-between py-6 x ' >
-        <div id="logo" className=''>
-          <img src="https://img.icons8.com/?size=80&id=EEBZaOS0K9cE&format=png" alt=""  className="h-20 pl-3 invert "/>
+        <div id="logo" >
+            <img src= {logoo} height={150} width={150} alt="" />
         </div>
         <div id="social" className="invert flex gap-3 pr-5 ">
-          <a href="https://www.linkedin.com/in/hardiklalwani/" ><img src="https://img.icons8.com/?size=50&id=32292&format=png" alt="" className="h-7"/></a>
-          <a href="https://www.instagram.com/sometimeshardik/"><img src="https://img.icons8.com/?size=50&id=8808&format=png" alt="" className="h-7"/></a>
+          <a href="https://www.instagram.com/sometimeshardik/" ><img src="https://img.icons8.com/?size=50&id=32292&format=png" alt="" className="h-7"/></a>
+          <a href="https://www.linkedin.com/in/hardiklalwani/"><img src="https://img.icons8.com/?size=50&id=8808&format=png" alt="" className="h-7"/></a>
           <a href="https://github.com/Hardikworkz"><img src="https://img.icons8.com/?size=50&id=12599&format=png" alt="" className="h-7"/></a>
         </div>
       </nav>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,10 +1,16 @@
+import p1 from "../components/download (1).jpeg";
+// import p2 from "../components/OIP (1).jpeg";
+import p3 from "../components/OIP (2).jpeg";
+import p4 from "../components/OIP (3).jpeg";
+import p5 from "../components/OIP (4).png";
+
 
 export const PROJECTS = [
     
     {
         title: "Portfolio Website",
         // location:<a href=""></a>,
-        Image: "download(1) ",   
+        Image: p1,   
         description:
           "A personal portfolio website showcasing projects, skills, and contact information.",
         technologies: ["HTML", "Tailwind CSS", "React", "JavaScript"],
@@ -12,7 +18,7 @@ export const PROJECTS = [
       {
         title: "Signup/Login Page",
         // location:<a href=""></a>,
-        Image: "OIP(2)",   
+        Image: p3,   
         description:"An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
         technologies: ["HTML", "CSS", "JavaScript", "React","Tailwind Css"],
       },
@@ -20,7 +26,7 @@ export const PROJECTS = [
     {
       title: "Tic-Tac-Toe",
       // location:<a href=""></a> ,
-      Image: " OIP(3) ",   
+      Image:p4,   
       description:
         "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
       technologies: ["HTML", "CSS", "JavaScript"],
@@ -28,7 +34,7 @@ export const PROJECTS = [
     {
       title: "Image Slider",
     //  location:<a href=""></a>,
-    Image: "Untitled design.png ",   
+    Image:p5,   
       description:"A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
       technologies: ["HTML", "CSS","Javascript"],
     },
